c894d9f015fcc8c39eb7d810412f3d241112a0a6
vanlantokutei <vovanlan.betoya@gmail.com>
2026-08-16 19:53:08 +0900

    Route notification settings pages

--- tokutei_site/urls.py
+++ tokutei_site/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("notifications_app.urls")),
     path("", include("study.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
