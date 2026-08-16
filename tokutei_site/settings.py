379ac11f108bf3a67e5002107618903c3285c689
vanlantokutei <vovanlan.betoya@gmail.com>
2026-08-16 22:32:08 +0900

    Use Resend backend for Django email

--- tokutei_site/settings.py
+++ tokutei_site/settings.py
@@ -51,13 +51,13 @@ STATIC_ROOT=BASE_DIR / 'staticfiles'
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
 
-EMAIL_BACKEND=os.getenv('EMAIL_BACKEND','django.core.mail.backends.smtp.EmailBackend')
+EMAIL_BACKEND=os.getenv('EMAIL_BACKEND','tokutei_site.resend_backend.ResendEmailBackend')
 EMAIL_HOST=os.getenv('EMAIL_HOST','smtp.gmail.com')
 EMAIL_PORT=int(os.getenv('EMAIL_PORT','587'))
 EMAIL_USE_TLS=os.getenv('EMAIL_USE_TLS','true').lower() == 'true'
 EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER','')
 EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD','')
-DEFAULT_FROM_EMAIL=os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER or 'noreply@onthitokutei.com')
+DEFAULT_FROM_EMAIL=os.getenv('DEFAULT_FROM_EMAIL','Ôn Thi Tokutei <noreply@onthitokutei.com>')
 ADMIN_NOTIFICATION_EMAIL=os.getenv('ADMIN_NOTIFICATION_EMAIL', EMAIL_HOST_USER)
 EMAIL_TIMEOUT=int(os.getenv('EMAIL_TIMEOUT','20'))
 
