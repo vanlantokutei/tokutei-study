d46bc6507a745a3f180ddebda4b1948a3b03c7ae
vanlantokutei <vovanlan.betoya@gmail.com>
2026-08-17 23:43:11 +0900

    Route JLPT N5 vocabulary lesson 3

--- study/urls.py
+++ study/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('jlpt/n5/alphabet/', TemplateView.as_view(template_name='study/jlpt_n5_alphabet.html'), name='jlpt_n5_alphabet'),
     path('jlpt/n5/vocabulary/', TemplateView.as_view(template_name='study/jlpt_n5_vocabulary.html'), name='jlpt_n5_vocabulary'),
     path('jlpt/n5/vocabulary/lesson-2/', TemplateView.as_view(template_name='study/jlpt_n5_vocabulary_lesson2.html'), name='jlpt_n5_vocabulary_lesson2'),
+    path('jlpt/n5/vocabulary/lesson-3/', TemplateView.as_view(template_name='study/jlpt_n5_vocabulary_lesson3.html'), name='jlpt_n5_vocabulary_lesson3'),
     path('jlpt/n5/grammar/', TemplateView.as_view(template_name='study/jlpt_n5_grammar.html'), name='jlpt_n5_grammar'),
     path('jlpt/n5/grammar/lesson-1/', TemplateView.as_view(template_name='study/jlpt_n5_grammar_lesson1.html'), name='jlpt_n5_grammar_lesson1'),
     path('jlpt/<str:level>/', TemplateView.as_view(template_name='study/jlpt_level.html'), name='jlpt_level'),
