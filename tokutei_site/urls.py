e2568b097518370547045e33d7e77055ddbd1d5e
vanlantokutei <vovanlan.betoya@gmail.com>
2026-08-18 12:05:47 +0900

    Use admin-aware login redirect

--- tokutei_site/urls.py
+++ tokutei_site/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from study.auth_views import StaffAwareLoginView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/login/", StaffAwareLoginView.as_view(), name="login"),
     path("", include("notifications_app.urls")),
     path("", include("study.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
