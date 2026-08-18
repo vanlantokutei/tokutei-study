358d63aae03044a0564db263e9c74ecca847e67f
vanlantokutei <vovanlan.betoya@gmail.com>
2026-08-18 22:26:48 +0900

    Keep admin login on main site

--- study/auth_views.py
+++ study/auth_views.py
@@ -2,11 +2,9 @@ from django.contrib.auth.views import LoginView
 
 
 class StaffAwareLoginView(LoginView):
-    """Send staff/admin users to Django admin after logging in."""
+    """Keep all users, including admin/staff, on the main site after login."""
 
     template_name = "registration/login.html"
 
     def get_success_url(self):
-        if self.request.user.is_staff or self.request.user.is_superuser:
-            return "/admin/"
-        return super().get_success_url()
+        return "/"
